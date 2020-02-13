02c5182e9d57d82be4a47fe0da3ba6d6b5afc89c
reza <reza.b.e@hotmail.com>
2020-02-13 20:33:18 +0330

    made Fibonacci code concurrency safe

--- fibonacci/calculation_logic.go
+++ fibonacci/calculation_logic.go
@@ -2,41 +2,70 @@ package fibonacci
 
 import (
 	"context"
+	"github.com/RezaZahedi/Go-Gin/model/memo"
 	"github.com/RezaZahedi/Go-Gin/model/proto/fibo_model"
 	"math/big"
 )
 
 type FiboGenerator struct{
-	cache map[int]*big.Int
+	cache *memo.Memo
 }
 
 func NewFiboGenerator() *FiboGenerator {
-	return &FiboGenerator{cache: make(map[int]*big.Int)}
+	return &FiboGenerator{cache: memo.New(fiboTemp)}
 }
 
 func (g *FiboGenerator) GenerateNumber(ctx context.Context, req *fibo_model.Request, rsp *fibo_model.Response) error {
-	rsp.Output = Calulate(int(req.Input), g.cache)
+	rsp.Output = calculate(int(req.Input), g.cache)
 	return nil
 }
 
-func Calulate(input int, cache map[int]*big.Int) string {
+//func Calculate(input int, cache map[int]*big.Int) string {
+//	if input < 0 {
+//		panic("Calculate: input must be positive")
+//	}
+//	if input == 0 { return "0" }
+//
+//	one := big.NewInt(1)
+//	var fibo func(a int) *big.Int
+//	fibo = func(a int) *big.Int {
+//		if a == 1 || a == 2 {
+//			return one
+//		}
+//		b := new(big.Int)
+//		if b, ok := cache[a]; ok {
+//			return b
+//		}
+//		b.Add(fibo(a - 2), fibo(a - 1))
+//		cache[a] = b
+//		return b
+//	}
+//
+//	return fibo(input).String()
+//}
 
-	if input == 0 { return "0" }
+var fibo func(a int) *big.Int
 
+func fiboTemp(key int) interface{} {
+	ans := fibo(key)
+	return ans
+}
+
+func calculate(input int, m *memo.Memo) string {
 	one := big.NewInt(1)
-	var fibo func(a int) *big.Int
+
+	//m := New(fiboTemp)
+	//defer m.Close()
+
 	fibo = func(a int) *big.Int {
 		if a == 1 || a == 2 {
 			return one
 		}
 		b := new(big.Int)
-		if b, ok := cache[a]; ok {
-			return b
-		}
-		b.Add(fibo(a - 2), fibo(a - 1))
-		cache[a] = b
+		b.Add(m.Get(a-2).(*big.Int), m.Get(a-1).(*big.Int))
 		return b
 	}
 
 	return fibo(input).String()
 }
+
---
+++ model/memo/memo_test.go
@@ -0,0 +1,23 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package memo_test
+//
+//import (
+//	"github.com/RezaZahedi/Go-Gin/model/memo"
+//	"testing"
+//)
+//
+//var _httpGetBody = HTTPGetBody
+//
+//func Test(t *testing.T) {
+//	m := memo.New(_httpGetBody)
+//	defer m.Close()
+//	Sequential(t, m)
+//}
+//
+//func TestConcurrent(t *testing.T) {
+//	m := memo.New(_httpGetBody)
+//	defer m.Close()
+//	Concurrent(t, m)
+//}
